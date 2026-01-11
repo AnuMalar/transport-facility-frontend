3be1ddbe9f4906a162e65bfbda96be0693872b5b
Anu <malaranu19@gmail.com>
2026-01-12 00:13:30 +0530

    update routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -7,5 +7,5 @@ import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideHttpClient(),
-  provideRouter(routes), provideAnimationsAsync()]
+  provideRouter(routes, withHashLocation()), provideAnimationsAsync()]
 };
