02c155128d64e792649095d46240019103b5eec3
Anu <malaranu19@gmail.com>
2026-01-12 17:19:52 +0530

    design changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    production: false,
-    url: 'http://localhost:4000/api'
-    // production: true,
-    // url: "https://transport-facility-backend.onrender.com/api"
+    // production: false,
+    // url: 'http://localhost:4000/api'
+    production: true,
+    url: "https://transport-facility-backend.onrender.com/api"
 }
